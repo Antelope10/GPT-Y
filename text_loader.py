bd494cb47ac6ebb8e065e798420224d47d88d1b1
Antelope10 <86484092+Antelope10@users.noreply.github.com>
2023-05-02 11:03:38 -0500

    update

--- text_loader.py
+++ text_loader.py
@@ -17,42 +17,43 @@ def clean_text(text):
 
 train = ""
 select = input("dataset: ")
-batch = input("batch: ") + ".txt"
-if select == "openwebtext":
-    dataset = load_dataset("stas/openwebtext-10k")
-    train = dataset['train']
-    with open(batch, 'w', encoding='utf-8') as f:
-        for element in train:
-            f.write(element['text'])
-            f.write("\n")
-elif select == "wikitext":
-    dataset = load_dataset("wikitext")
-    train = dataset['train']
-    limit = int(input("# of examples"))
-    with open(batch, 'w', encoding='utf-8') as f:
-        for i in range(limit):
-            f.write(train[i]['text'])
-            f.write("\n")
-elif select == "articles":
-    dataset = load_dataset("cnn_dailymail", '3.0.0')
-    train = dataset['train']
-    with open(batch, 'w', encoding='utf-8') as f:
-        for element in train:
-            f.write(element['article'])
-            f.write("\n")
-elif select == "hate":
-    dataset = load_dataset("hate_speech18")
-    train = dataset['train']
-    with open(batch, 'w', encoding='utf-8') as f:
-        for element in train:
-            if element["label"] == 1:
-                f.write(element["text"])
-                f.write("\n")
-else:
-    text_batch = batch
-    for filepath in glob(text_batch):
+if select == "b":
+    batch = "texts/" + input("batch: ") + "/*.txt"
+    for filepath in glob(batch):
         with open(filepath, 'r', encoding='utf-8') as f:
             text = clean_text(f.read())
         train += text
     with open('train.txt', 'w', encoding = 'utf-8') as f:
         f.write(train)
+else: 
+    batch = input("filepath: ")
+    if select == "openwebtext":
+        dataset = load_dataset("stas/openwebtext-10k")
+        train = dataset['train']
+        with open(batch, 'w', encoding='utf-8') as f:
+            for element in train:
+                f.write(element['text'])
+                f.write("\n")
+    elif select == "wikitext":
+        dataset = load_dataset("wikitext")
+        train = dataset['train']
+        limit = int(input("# of examples"))
+        with open(batch, 'w', encoding='utf-8') as f:
+            for i in range(limit):
+                f.write(train[i]['text'])
+                f.write("\n")
+    elif select == "articles":
+        dataset = load_dataset("cnn_dailymail", '3.0.0')
+        train = dataset['train']
+        with open(batch, 'w', encoding='utf-8') as f:
+            for element in train:
+                f.write(element['article'])
+                f.write("\n")
+    elif select == "hate":
+        dataset = load_dataset("hate_speech18")
+        train = dataset['train']
+        with open(batch, 'w', encoding='utf-8') as f:
+            for element in train:
+                if element["label"] == 1:
+                    f.write(element["text"])
+                    f.write("\n")
